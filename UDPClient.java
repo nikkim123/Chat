a108bc332ec9385fb29e91d42b019c90910fba6c
neethu <neethupushpan01@gmail.com>
2018-04-04 12:29:14 +0530

    cm3

---
+++ UDPClient.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+class UDPClient
+{
+   public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser =
+      new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      boolean flag=true;
+      
